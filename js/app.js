2e50b85f9b4f44c7d570013b2e88c59524e73309
babujal <dercamar1@gmail.com>
2024-07-09 23:19:28 -0400

    Need bug fixed, coments in line 141

--- js/app.js
+++ js/app.js
@@ -25,22 +25,51 @@ let category
 let randomWord
 let guessedLettersArr = []
 let chart
+let roundPointsP1 = 0
+let roundPointsP2 = 0
+/////////////States/////////////////
 let shift = true
 let allowInput = true
+let winnerPlayerState
 /////////////Constants//////////////
 const button = document.querySelector('.btn')
 const uiCategory = document.querySelector('.category')
 const uiWordSpace = document.querySelector('.word')
 const lettersP1 = document.querySelector('.lettersP1')
 const lettersP2 = document.querySelector('.lettersP2')
+const scoreP1 = document.querySelector('.scoreP1')
+const scoreP2 = document.querySelector('.scoreP2')
 /////////////Suport for conditions///////////
 const compareChart = () => {
     return randomWord.toLowerCase().includes(chart)
 }
 /////////////Functions//////////////
+const scoreGiver = () => {
+    if (winnerPlayerState) {
+        scoreP1.innerText = '$'
+    }else {
+        scoreP2.innerText = '&'
+    }
+}
+
+const findTheWinner = () => {
+    if (roundPointsP1 > roundPointsP2) {
+        winnerPlayerState = true
+        scoreGiver()
+        uiCategory.innerText = 'Player One Wins'
+    }else if (roundPointsP1 < roundPointsP2) {
+        winnerPlayerState = false
+        scoreGiver()
+        uiCategory.innerText = 'Player Two Wins'
+    }else {
+        uiCategory.innerText = 'It is a tie'
+    }
+}
+
 const thereIsAWin = () => {
     if (guessedLettersArr.sort().join('') === randomWord.split('').sort().join('')) {
         allowInput = false
+        findTheWinner()
         console.log('There is a winner')
         return true
     }else{
@@ -106,13 +135,17 @@ const removeWordDivs = () => {
 
 const lookForCharOccurences = (domEl) => {
     const charactersDivs = document.querySelectorAll(domEl)
-    let count = 0
     charactersDivs.forEach(el => {
         if (el.innerText.toLowerCase() === chart) {
             el.classList.remove('hidden')
+            //Before pushing to guessedLettersArr I have to put a condition sayng that if guessedLettersArr does not includes that letter push it
             guessedLettersArr.push(el.innerText)
-            // console.log(guessedLettersArr.sort())
-            // console.log(randomWord.split('').sort())
+            if (shift) {
+                roundPointsP1 += 1
+            }else{
+                roundPointsP2 += 1
+            }
+            console.log(`Points P1: ${roundPointsP1}, Points P2: ${roundPointsP2}`)
         }
     })
 }
@@ -144,13 +177,17 @@ const init = () => {
 
 const handleClick = () => {
     removeWordDivs()
+    guessedLettersArr = []
+    roundPointsP1 = 0
+    roundPointsP2 = 0
+    allowInput = true
     init()
 }
 
 ////////////////Event Listeners///////
 document.addEventListener('keydown', (e) => {
     chart = e.key
-    
+
     if (allowInput === true) {
         if (compareChart()) {
             console.log(`The letter ${chart} is present`)
